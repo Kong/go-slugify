b0b749c1641896891f44351c31b203c334d7cfe1
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-10-23 16:38:53 +0200

    fix(slugifier): make Slugifier exported, fix initialization
    also adds test cases where the replacement chars are empty.

--- slugify.go
+++ slugify.go
@@ -9,57 +9,77 @@ import (
 )
 
 const (
-	defaultToLower                = true
 	defaultWordSeparator          = "-"
 	defaultInvalidCharReplacement = "-"
 )
 
-type slugifier struct {
-	toLower                bool   // convert to lower or not
-	wordSeparator          string // separator char between words
-	invalidCharReplacement string // replacement for illegal chars
+type Slugifier struct {
+	skipToLower            bool    // convert to lower or not
+	wordSeparator          *string // separator char between words
+	invalidCharReplacement *string // replacement for illegal chars
 	reInValidChar          *regexp.Regexp
 	reDupSeparatorChar     *regexp.Regexp
+	initialized            bool
 }
 
 // NewSlugifier creates a new slugifier. Defaults to lowercasing and using a dash ("-") as the word separator and
 // as the invalid character replacement.
-func NewSlugifier() slugifier {
-	s := slugifier{
-		toLower:                defaultToLower,
-		invalidCharReplacement: defaultInvalidCharReplacement,
-	}
+func NewSlugifier() Slugifier {
+	s := Slugifier{}
+	s.InvalidChar(defaultInvalidCharReplacement)
 	s.WordSeparator(defaultWordSeparator)
 	return s
 }
 
+// initialize initializes the slugifier. This is called automatically by Slugify() if it hasn't already been called.
+func (s *Slugifier) initialize() {
+	if s.invalidCharReplacement == nil {
+		s.InvalidChar(defaultInvalidCharReplacement)
+	}
+
+	if s.wordSeparator == nil {
+		ws := defaultWordSeparator
+		s.wordSeparator = &ws
+	}
+
+	separatorForRe := regexp.QuoteMeta(*s.wordSeparator)
+	s.reInValidChar = regexp.MustCompile(fmt.Sprintf("[^%sa-zA-Z0-9]", separatorForRe))
+	if separatorForRe != "" {
+		s.reDupSeparatorChar = regexp.MustCompile(fmt.Sprintf("%s{2,}", separatorForRe))
+	} else {
+		s.reDupSeparatorChar = nil
+	}
+
+	s.initialized = true
+}
+
 // ToLower sets the flag indicating if the slugified result should be lowercased.
-func (s *slugifier) ToLower(toLower bool) {
-	s.toLower = toLower
+// Returns the slugifier for easy chaning.
+func (s *Slugifier) ToLower(toLower bool) *Slugifier {
+	s.skipToLower = !toLower
+	return s
 }
 
 // WordSeparator sets the word separator character to use. Defaults to a dash ("-"). The word separator is used to
 // replace whitespace. Leading and trailing word separators are trimmed. Multiple successive word separators are
 // replaced with a single word separator.
-func (s *slugifier) WordSeparator(wordSeparator string) error {
-	s.wordSeparator = wordSeparator
-	separatorForRe := regexp.QuoteMeta(s.wordSeparator)
-	var err error
-	if s.reInValidChar, err = regexp.Compile(fmt.Sprintf("[^%sa-zA-Z0-9]", separatorForRe)); err != nil {
-		return err
-	}
-	if s.reDupSeparatorChar, err = regexp.Compile(fmt.Sprintf("%s{2,}", separatorForRe)); err != nil {
-		return err
-	}
-	return nil
+// Returns the slugifier for easy chaning.
+func (s *Slugifier) WordSeparator(wordSeparator string) *Slugifier {
+	s.wordSeparator = &wordSeparator
+	s.reInValidChar = nil
+	s.reDupSeparatorChar = nil
+	s.initialized = false
+	return s
 }
 
-// InvalidCharReplacement sets the character to use to replace invalid characters (anything not a-z, A-Z, 0-9, the
-// word separator, or the InvalidCharReplacement). Defaults to a dash ("-"). Leading and trailing
+// InvalidChar sets the character to use to replace invalid characters (anything not a-z, A-Z, 0-9, the
+// word separator, or the InvalidChar). Defaults to a dash ("-"). Leading and trailing
 // InvalidCharReplacements are trimmed. Multiple successive InvalidCharReplacements are NOT replaced with a single
-// InvalidCharReplacement.
-func (s *slugifier) InvalidCharReplacement(invalidCharReplacement string) {
-	s.invalidCharReplacement = invalidCharReplacement
+// InvalidChar.
+// Returns the slugifier for easy chaning.
+func (s *Slugifier) InvalidChar(invalidCharReplacement string) *Slugifier {
+	s.invalidCharReplacement = &invalidCharReplacement
+	return s
 }
 
 // Version return version
@@ -69,23 +89,28 @@ func Version() string {
 
 // Slugify implements making a pretty slug from the given text.
 // e.g. Slugify("kožušček hello world") => "kozuscek-hello-world"
-func (s *slugifier) Slugify(txt string) string {
+func (s *Slugifier) Slugify(txt string) string {
+	if !s.initialized {
+		s.initialize()
+	}
 	txt = unidecode.Unidecode(txt)
-	txt = strings.Join(strings.Fields(txt), s.wordSeparator)
-	txt = s.reInValidChar.ReplaceAllString(txt, s.invalidCharReplacement)
-	txt = s.reDupSeparatorChar.ReplaceAllString(txt, s.wordSeparator)
+	txt = strings.Join(strings.Fields(txt), *s.wordSeparator)
+	txt = s.reInValidChar.ReplaceAllString(txt, *s.invalidCharReplacement)
+	if s.reDupSeparatorChar != nil {
+		txt = s.reDupSeparatorChar.ReplaceAllString(txt, *s.wordSeparator)
+	}
 
 	// trim leading and trailing word separators and invalidCharReplacements
 	for len(txt) > 0 &&
-		(string(txt[0]) == s.wordSeparator || string(txt[0]) == s.invalidCharReplacement) {
+		(string(txt[0]) == *s.wordSeparator || string(txt[0]) == *s.invalidCharReplacement) {
 		txt = txt[1:]
 	}
 	for len(txt) > 0 &&
-		(string(txt[len(txt)-1]) == s.wordSeparator || string(txt[len(txt)-1]) == s.invalidCharReplacement) {
+		(string(txt[len(txt)-1]) == *s.wordSeparator || string(txt[len(txt)-1]) == *s.invalidCharReplacement) {
 		txt = txt[:len(txt)-1]
 	}
 
-	if s.toLower {
+	if !s.skipToLower {
 		txt = strings.ToLower(txt)
 	}
 	return txt
--- slugify_test.go
+++ slugify_test.go
@@ -9,7 +9,7 @@ type testCase struct {
 	input, expect string
 }
 
-func testSlugify(t *testing.T, slugifier slugifier, input, expect string) {
+func testSlugify(t *testing.T, slugifier Slugifier, input, expect string) {
 	log.Printf("input %v, expect %v", input, expect)
 	ret := slugifier.Slugify(input)
 	if ret != expect {
@@ -17,7 +17,7 @@ func testSlugify(t *testing.T, slugifier slugifier, input, expect string) {
 	}
 }
 
-func testSlugifyCases(t *testing.T, slugifier slugifier, cases []testCase) {
+func testSlugifyCases(t *testing.T, slugifier Slugifier, cases []testCase) {
 	for _, c := range cases {
 		testSlugify(t, slugifier, c.input, c.expect)
 	}
@@ -84,6 +84,24 @@ func TestSlugifyWordSeparator(t *testing.T) {
 	testSlugifyCases(t, slugifier, cases)
 }
 
+func TestSlugifyEmptyWordSeparator(t *testing.T) {
+	cases := []testCase{
+		{"", ""},
+		{"abc", "abc"},
+		{"abc234", "abc234"},
+		{"This is a test ---", "thisisatest"}, // trailing separator+invalid removed
+		{"_-_This is a test", "thisisatest"},  // leading separator+invalid removed
+		{"This -- is \t\t  \r\n a ## test ---", "this--isa--test"}, // successive whitespace is reduced to 1
+		{"北京kožušček", "beijingkozuscek"},
+		{"Nín hǎo. Wǒ shì zhōng guó rén", "ninhao-woshizhongguoren"},
+		{`C\'est déjà l\'été.`, "c--estdejal--ete"},
+	}
+
+	slugifier := NewSlugifier()
+	slugifier.WordSeparator("")
+	testSlugifyCases(t, slugifier, cases)
+}
+
 func TestSlugifyInvalidCharReplacement(t *testing.T) {
 	cases := []testCase{
 		{"", ""},
@@ -98,7 +116,44 @@ func TestSlugifyInvalidCharReplacement(t *testing.T) {
 	}
 
 	slugifier := NewSlugifier()
-	slugifier.InvalidCharReplacement("_")
+	slugifier.InvalidChar("_")
+	testSlugifyCases(t, slugifier, cases)
+}
+
+func TestSlugifyEmptyInvalidCharReplacement(t *testing.T) {
+	cases := []testCase{
+		{"", ""},
+		{"abc", "abc"},
+		{"abc234", "abc234"},
+		{"This is a test ---", "this-is-a-test"}, // trailing separator+invalid removed
+		{"_-_This is a test", "this-is-a-test"},  // leading separator+invalid removed
+		{"This -- is \t\t  \r\n a ## test ---", "this-is-a-test"}, // successive whitespace is reduced to 1
+		{"北京kožušček", "bei-jing-kozuscek"},
+		{"Nín hǎo. Wǒ shì zhōng guó rén", "nin-hao-wo-shi-zhong-guo-ren"},
+		{`C\'est déjà l\'été.`, "cest-deja-lete"},
+	}
+
+	slugifier := NewSlugifier()
+	slugifier.InvalidChar("")
+	testSlugifyCases(t, slugifier, cases)
+}
+
+func TestSlugifyEmptyWordSeparatorAndInvalidCharReplacement(t *testing.T) {
+	cases := []testCase{
+		{"", ""},
+		{"abc", "abc"},
+		{"abc234", "abc234"},
+		{"This is a test ---", "thisisatest"},
+		{"_-_This is a test", "thisisatest"},
+		{"This -- is \t\t  \r\n a ## test ---", "thisisatest"},
+		{"北京kožušček", "beijingkozuscek"},
+		{"Nín hǎo. Wǒ shì zhōng guó rén", "ninhaowoshizhongguoren"},
+		{`C\'est déjà l\'été.`, "cestdejalete"},
+	}
+
+	slugifier := NewSlugifier()
+	slugifier.InvalidChar("")
+	slugifier.WordSeparator("")
 	testSlugifyCases(t, slugifier, cases)
 }
 
@@ -109,7 +164,19 @@ func TestSlugifyReplacementsBecomeValidCharacters(t *testing.T) {
 	}
 
 	slugifier := NewSlugifier()
-	slugifier.InvalidCharReplacement("#")
+	slugifier.InvalidChar("#")
 	slugifier.WordSeparator("*") // valid; but still duplicates removed!
 	testSlugifyCases(t, slugifier, cases)
+}
+
+func TestSlugifyChainingSetup(t *testing.T) {
+	cases := []testCase{
+		{"This -- is \t\t  \r\n a ## test ---", "This*##*is*a*##*test"},
+	}
+
+	slugifier := (&Slugifier{}).ToLower(false).InvalidChar("#").WordSeparator("*")
+
+	slugifier.InvalidChar("#")
+	slugifier.WordSeparator("*")
+	testSlugifyCases(t, *slugifier, cases)
 }
